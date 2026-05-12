695a16f3db9e8e2e0f166c7a4b2f5d53e7e162db
Tossah Fafa <fafatossah@gmail.com>
2026-05-12 19:21:34 +0000

    Modification des commentaire

--- src/Devinette.java
+++ src/Devinette.java
@@ -14,13 +14,13 @@ public class Devinette {
         System.out.println("Bienvenue dans les devinette! J'ai choisi un nombre secret entre 1 et 100.");
         System.out.println("Tu as 10 essais pour le Deviner.");
 
-        //  Création d'une boucle
+        //  Création d'une  boucle
         while (essais < tentativesMax) {
             essais++;
             System.out.print("Essai n°" + essais + " - Devine le nombre secret, entre le !! : ");
 
         }
-        //  Lecture de  l'entrée de l'utilisateur
+        //  Lecture de l'entrée de l'utilisateur (Le joueur)
 
         int devinette = scanner.nextInt();
 
@@ -38,7 +38,7 @@ public class Devinette {
             System.out.println("C'est supérieur au nombre secret!");
         }
 
-    // Résultat en cas de victoire
+    // Résultat en cas de victoire du joueur
         if (Gagner) {
         System.out.println("Félicitations ! Tu as trouvé le nombre secret  " + nombreSecret + " en " + essais + " essais.");
     } else {
