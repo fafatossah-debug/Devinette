e9382e5c0905a2647617fa96361e973bfd5ecefe
Tossah Fafa <fafatossah@gmail.com>
2026-05-12 18:50:37 +0000

    Déclaration des variables et génération du nombre aléatoire

--- src/Devinette.java
+++ src/Devinette.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Devinette {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        // Génération d'un nombre aléatoire entre 1 et 100
+        int nombreSecret = random.nextInt(100) + 1;
+        int tentativesMax = 10;
+        int essais = 0;
+        boolean Gagner = false;
+
+        System.out.println("Bienvenue dans les devinette! J'ai choisi un nombre aléatoire entre 1 et 100.");
+        System.out.println("Tu as 10 essais pour le Deviner.");
+    }
 }
