1c76c0595e513c9b717da3b45168d08e3c4a6411
Tossah Fafa <fafatossah@gmail.com>
2026-05-12 19:19:52 +0000

    Boucle ^pour les 10 essaie

--- src/Devinette.java
+++ src/Devinette.java
@@ -11,19 +11,45 @@ public class Devinette {
         int essais = 0;
         boolean Gagner = false;
 
-        System.out.println("Bienvenue dans les devinette! J'ai choisi un nombre aléatoire entre 1 et 100.");
+        System.out.println("Bienvenue dans les devinette! J'ai choisi un nombre secret entre 1 et 100.");
         System.out.println("Tu as 10 essais pour le Deviner.");
 
         //  Création d'une boucle
         while (essais < tentativesMax) {
             essais++;
-            System.out.print("Essai n°" + essais + " - Entre ta devinette : ");
+            System.out.print("Essai n°" + essais + " - Devine le nombre secret, entre le !! : ");
 
         }
         //  Lecture de  l'entrée de l'utilisateur
 
         int devinette = scanner.nextInt();
 
+        //  Comparaison du nombre secret et celui du joueur
 
+        if (devinette == nombreSecret) {
+            // Cas de victoire
+
+            Gagner = true;
+
+            //Les indices
+        } else if (devinette < nombreSecret) {
+            System.out.println("C'est inferieur au nombre secret !");
+        } else {
+            System.out.println("C'est supérieur au nombre secret!");
+        }
+
+    // Résultat en cas de victoire
+        if (Gagner) {
+        System.out.println("Félicitations ! Tu as trouvé le nombre secret  " + nombreSecret + " en " + essais + " essais.");
+    } else {
+        // Résulutat en cas de défaite après 10 essais
+        System.out.println("Dommage, tu as épuisé tes 10 essais.");
+        System.out.println("Le nombre secret était : " + nombreSecret);
     }
+
+        scanner.close();
+}
+
+
+
 }
